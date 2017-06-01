63a84f52df5fcda710583a797a63600e0a032c4f
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-31 23:02:56 -0700

    added first route

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ var express = require('express'),
 mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set("view engine","ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
