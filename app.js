1cc259b54e910293ee9df387ce2a186ca5aec0ef
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-31 23:45:51 -0700

    added config

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 /**
- * Created by Rakshith on 5/31/2017.
+ * Express JS starting point that loads
+ * Express, Mongoose, Body - Parser.
  */
 var express = require('express'),
     app = express(),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
+
+//Config setup
 mongoose.connect('mongodb://localhost/restful_blog_app');
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+/**
+ * Setting up the DB model with the schema.
+ */
+var Blog = mongoose.model("Blog", {
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now()}
+});
+
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
